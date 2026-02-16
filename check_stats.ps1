2a0fe7463f35a98d5bb353fcbeccb6a0ff007d5c
maulidi-zikri <nmaulidizikri14@gmail.com>
2026-02-16 20:20:42 +0700

    Delete check_stats.ps1

--- check_stats.ps1
+++
@@ -1,6 +0,0 @@
-$content = Get-Content 'c:/Users/IP330.290/Downloads/rasionalisasi_utbk/ptn_data.js' -Raw
-$matchesNumeric = [regex]::Matches($content, '"minScore":\s*"[0-9]+"')
-$matchesMissing = [regex]::Matches($content, '"minScore":\s*"belum ada data"')
-
-Write-Host "Numeric minScore count: $($matchesNumeric.Count)"
-Write-Host "Missing minScore count: $($matchesMissing.Count)"
